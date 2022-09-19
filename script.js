2d284fa4c585fcf24120961da7b9b4b80694e201
niranjanrai <rainiranjan1998@gmail.com>
2022-09-19 16:22:54 +0530

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-'use strict';
+// 'use strict';
 // console.log(document.querySelector('.message').textContent);
+// function first() {
+//   console.log('first');
+//   second();
+// }
+// function second() {
+//   console.log('second');
+//   third();
+// }
+// function third() {
+//   console.log('third');
+//   fourth();
+// }
+// function fourth() {
+//   console.log('fourth');
+// }
+
+// first();
+
+var x = 10;
+var y = 15;
+
+console.log(getSum(x, y));
+function getSum(a, b) {
+  return a + b;
+}
