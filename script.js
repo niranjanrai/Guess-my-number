7636442aad20cd40c265577370bbcea184150178
niranjanrai <rainiranjan1998@gmail.com>
2022-09-20 11:59:15 +0530

    first commit

--- script.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 correct number';
-console.log(document.querySelector('.message').textContent);
