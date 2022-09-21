1630dd927c1c0c03df362d97cabbcbb88625a44d
niranjanrai <rainiranjan1998@gmail.com>
2022-09-21 14:47:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+/* 
+document.querySelector('.message').textContent = '🎉 correct number';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 50;
+console.log(document.querySelector('.guess').value);
+const guessBtn = (document.querySelector('.guess').value = 30);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+  // document.querySelector('.message').textContent = '🎉 correct number';
+});
