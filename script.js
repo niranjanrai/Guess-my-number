eecac3e0d43d979e9b3d1a37ad52ab43e98f675d
niranjanrai <rainiranjan1998@gmail.com>
2022-10-10 15:01:51 +0530

    first commit

--- script.js
+++ script.js
@@ -6,11 +6,40 @@ document.querySelector('.score').textContent = 50;
 console.log(document.querySelector('.guess').value);
 const guessBtn = (document.querySelector('.guess').value = 30);
 */
-const x = function () {
-  console.log(23);
-};
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
-  document.querySelector('.message').textContent = '🎉 correct number';
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number 🎉🎉';
+    document.body.style.background = 'green';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        ' 😭😭 You lost the game';
+      document.body.style.background = 'red';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        ' 😭😭 You lost the game';
+      document.querySelector('.score').textContent = 0;
+      document.body.style.background = 'red';
+    }
+  }
 });
