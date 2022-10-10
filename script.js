be9d0a9ff097a3acdee04140712cde724cdba378
niranjanrai <rainiranjan1998@gmail.com>
2022-10-10 15:13:52 +0530

    first commit

--- script.js
+++ script.js
@@ -9,16 +9,22 @@ const guessBtn = (document.querySelector('.guess').value = 30);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number 🎉🎉';
-    document.body.style.background = 'green';
+    document.querySelector('.number').textContent = secretNumber;
+    document.body.style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high';
@@ -30,6 +36,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.body.style.background = 'red';
       document.querySelector('.score').textContent = 0;
     }
+    // when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low';
