9b6e6b8b2089f57b9372e8c502fbf08b34ea9e2a
niranjanrai <rainiranjan1998@gmail.com>
2022-10-11 21:05:01 +0530

    first commit

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
-const again = document.querySelector('.again');
+// const again = document.querySelector('.again');
 again.addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
